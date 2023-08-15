086ed190d455d1cf8280ec061b54de9b93f1d11e
sINFdorako <dominik.shp@googlemail.com>
2023-08-15 21:09:53 +0200

    new admin titel

--- node-app/main.ts
+++ node-app/main.ts
@@ -4,7 +4,7 @@ const app: express.Application = express();
 const port: number = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+    res.send('Admin Panel - Fotogalerie Wolfram Wildner');
 });
 
 app.listen(port, () => {
