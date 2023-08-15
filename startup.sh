60278a131e74cec1a972502028de27865c475601
sINFdorako <dominik.shp@googlemail.com>
2023-08-15 14:27:19 +0200

    added configs and node

--- startup.sh
+++ startup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-#
-# before starting the script, make sure to set the correct ENVIRONEMNT 
-# 'export ENVIRONMENT=production' or export 'ENVIRONMENT=local'
-#
-# reason for this script is to not suffer from letsencrypt issues locally 
-# and also to not have any issues on the server when running ssl
-#
-# check for envirnoment variable "ENVIRONMENT"
-if [ "$ENVIRONMENT" == "production" ]; then
-    cp nginx-config/nginx-prod.conf nginx-config/default.conf
-else
-    cp nginx-config/nginx-local.conf nginx-config/default.conf
+
+# Check if the ENVIRONMENT variable exists
+if [ -z "$ENVIRONMENT" ]; then
+    echo "Error: The ENVIRONMENT variable is not set."
+    echo "Please set it to either 'production' or 'local'."
+    echo "For example: 'export ENVIRONMENT=production' or 'export ENVIRONMENT=local'"
+    exit 1
 fi
 
-# start containers
-podman-compose up
+# Set nginx configuration based on the ENVIRONMENT variable
+if [ "$ENVIRONMENT" == "production" ]; then
+    cp nginx-config/nginx-prod.conf nginx-config/default.conf
+elif [ "$ENVIRONMENT" == "local" ]; then
+    cp nginx-config/nginx-local.conf nginx-config/default.conf
+else
+    echo "Error: Invalid ENVIRONMENT value. Please set it to either 'production' or 'local'."
+    exit 1
+fi
+
+# Build node image only if it doesn't exist already
+if [ -z "$(docker images -q custom-node-image)" ]; then
+    docker build -t custom-node-image -f Dockerfile.node .
+fi
+
+# Start containers
+docker-compose up
