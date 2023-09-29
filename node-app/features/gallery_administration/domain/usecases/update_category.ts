f7acd69f9ddea97382dba6315d8e9211d497ddfe
sINFdorako <dominik.shp@googlemail.com>
2023-09-29 16:15:25 +0200

    updated category route

--- node-app/features/gallery_administration/domain/usecases/update_category.ts
+++ node-app/features/gallery_administration/domain/usecases/update_category.ts
@@ -4,7 +4,7 @@ import { ICategoryRepository } from "../repositories/icategory_repository";
 export class UpdateCategory {
     constructor(private categoryRepository: ICategoryRepository) {};
 
-    async execute(userId: number, categoryId: number, title: string, description?: string): Promise<Category> {
-        return await this.categoryRepository.updateCategory(userId, categoryId, description ?? '');
+    async execute(userId: number, title: string, categoryId: number,  description?: string): Promise<Category> {
+        return await this.categoryRepository.updateCategory(userId, title, categoryId, description ?? '');
     }
 }
